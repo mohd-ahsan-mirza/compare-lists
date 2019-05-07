b58904ea01b4ce68ccd5b6adb66d5c26a3039544
Ahsan <mohd.ahsan.mirza@gmail.com>
2019-05-07 17:30:26 -0400

    Updates

---
+++ precache-manifest.4774fd91dd7de570680f38b1baff7e8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14d558136bfe514c6ffea2a75b702135",
+    "url": "/compare-lists/index.html"
+  },
+  {
+    "revision": "48e2e8b6dbd1d8957894",
+    "url": "/compare-lists/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/compare-lists/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "48e2e8b6dbd1d8957894",
+    "url": "/compare-lists/static/js/main.3bd53245.chunk.js"
+  },
+  {
+    "revision": "0c5dd1b1c3ca6ad526a9",
+    "url": "/compare-lists/static/js/runtime~main.670bf5f7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/compare-lists/static/media/logo.5d5d9eef.svg"
+  }
+]);
